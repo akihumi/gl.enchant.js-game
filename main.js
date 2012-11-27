51cea839021a25d49087d0976ee1546182d9f972
Akifumi Tamashiro <s11013@std.it-college.ac.jp>
2012-11-27 16:09:08 +0900

    add palyer shoot

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ enchant();
 var game;
 window.addEventListener('load', function(){
     game = new Game(640, 640);
-    game.preload('enchant.png', 'tibi.png', 'earth.png');
+    game.preload('tibi.png', 'earth.png');
     game.fps = 60;
     game.addEventListener('load', function(){
         // create 3D scene
@@ -18,17 +18,23 @@ window.addEventListener('load', function(){
         camera.centerZ = 200;
         scene.setCamera(camera);
         // create Sphere
-        // for(var i = 0; i < 100; i++){
-        //     new Obstacle(scene, camera);
-        // }
+        // 中心
+        var sphere5 = new Sphere();
+        sphere5.x = 0; sphere5.y = 0; sphere5.z = 50;
+        sphere5.mesh.setBaseColor("#ffffff");
+        scene.addChild(sphere5);
+        game.keybind(32, "a");
         game.rootScene.addEventListener('enterframe', function(e){
             var input = game.input;
-            if(input.left){ camera.sidestep(0.1); camera.x += 0.1;}
-            if(input.right){ camera.sidestep(-0.1); camera.x -= 0.1; }
-            if(input.up){ camera.altitude(0.1); camera.y += 0.1; }
-            if(input.down){ camera.altitude(-0.1); camera.y -= 0.1; }
+            if(input.left){ camera.sidestep(0.1); camera.x += 1; camera.centerX = camera.x;}
+            if(input.right){ camera.sidestep(-0.1); camera.x -= 1; camera.centerX = camera.x;}
+            if(input.up){ camera.altitude(0.1); camera.y += 1; camera.centerY = camera.y;}
+            if(input.down){ camera.altitude(-0.1); camera.y -= 1; camera.centerY = camera.y;}
             if(game.frame % 60 == 0){
-                new Obstacle(scene, camera);
+                new Obstacle(scene, camera, camera.x, camera.y);
+            }
+            if(input.a){
+                new Shot(scene, camera);
             }
             // if(input.left){  camera.centerX += 0.1;}
             // if(input.right){  camera.centerX -= 0.1; }
@@ -42,6 +48,7 @@ window.addEventListener('load', function(){
 var Obj = enchant.Class.create(enchant.gl.primitive.Sphere, {
     initialize: function(scene){
         enchant.gl.primitive.Sphere.call(this);
+        this.az = 0;
         this.theta = 0;
         this.matrix = new mat4.create();
         this.mesh.ambient = [0.3, 0.3, 0.3, 1.0];
@@ -62,20 +69,34 @@ var Obj = enchant.Class.create(enchant.gl.primitive.Sphere, {
     }
 });
 var Obstacle = enchant.Class.create(Obj, {
-    initialize: function(scene, camera){
+    initialize: function(scene, camera, x, y){
         Obj.call(this, scene);
-        this.az = 0;
-        this.x = camera.x;
-        this.y = camera.y;
+        this.x = x;
+        this.y = y;
         this.z = 200;
-        this.matrix = new mat4.create();
-        // this.mesh.setBaseColor("#f18b8c");
         // テクスチャをつける
         this.mesh.texture = new Texture('earth.png');
         this.addEventListener('enterframe', function(e){
             this.az -= 0.1;
             this.z += this.az;
             if(this.z < camera.z){
+                this.remove(scene);
+            }
+        });
+    }
+});
+
+var Shot = enchant.Class.create(Obj, {
+    initialize: function(scene, camera){
+        Obj.call(this, scene);
+        this.mesh.setBaseColor("#f18b8c");
+        this.x = camera.x;
+        this.y = camera.y;
+        this.z = camera.z + 5;
+        this.addEventListener('enterframe', function(e){
+            this.az += 1;
+            this.z += this.az;
+            if(this.z > 500){
                 this.remove(scene);
             }
         });
